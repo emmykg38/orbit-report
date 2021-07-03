6b2efa612a08b0f75bec8deb8a1e36cae0bfbbe4
millylane003 <emilykgerst19@gmail.com>
2021-07-03 10:28:40 -0500

    fixed json; spaceDebris() and sort is now correctly working!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,31 +15,18 @@ export class AppComponent {
 
   constructor() {
     this.sourceList = [];
-    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+    this.displayList = [];
+    // this.search;
  
     window.fetch(satellitesUrl).then(function(response) {
        response.json().then(function(data) {
  
           let fetchedSatellites = data.satellites;
-          let table = document.getElementById("table");
-          let index = 0;
-          for (let index in fetchedSatellites) {
-            table.innerHTML += `
-            <td>${fetchedSatellites[index].name}</td>
-            <td [class.warning]="satellite.shouldShowWarning()">${fetchedSatellites[index].type}</td>
-            <td>${fetchedSatellites[index].operational}</td>
-            <td>${fetchedSatellites[index].orbitType}</t d>
-            <td>${fetchedSatellites[index].launchDate}</td>
-            `
-          }
-
-
-          // let space = new Satellite(fetchedSatellites[7].name, fetchedSatellites[7].type, fetchedSatellites[7].launchDate, fetchedSatellites[7].orbitType, fetchedSatellites[7].operational);
-
-          // this.sourceList.push(space);
-
- 
+          for (let i = 0; i < fetchedSatellites.length; i++) {
+            let sats = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational)
+            this.sourceList.push(sats);
+          } 
           this.displayList = this.sourceList.slice(0);
        }.bind(this));
     }.bind(this));
@@ -57,11 +44,9 @@ export class AppComponent {
           matchingSatellites.push(this.sourceList[i]);
        }
     }
-  //   // assign this.displayList to be the array of matching satellites
-    // this will cause Angular to re-make the table, but now only containing matches
     this.displayList = matchingSatellites;
   }
-  
+
 }
 
 
@@ -70,7 +55,22 @@ export class AppComponent {
 
 
 
+          // let table = document.getElementById("table");
+          // let index = 0;
+          // for (let index in fetchedSatellites) {
+          //   table.innerHTML += `
+          //   <td>${fetchedSatellites[index].name}</td>
+          //   <td [class.warning]="satellite.shouldShowWarning()">${fetchedSatellites[index].type}</td>
+          //   <td>${fetchedSatellites[index].operational}</td>
+          //   <td>${fetchedSatellites[index].orbitType}</t d>
+          //   <td>${fetchedSatellites[index].launchDate}</td>
+          //   `
+          // }
 
+
+          // let space = new Satellite(fetchedSatellites[7].name, fetchedSatellites[7].type, fetchedSatellites[7].launchDate, fetchedSatellites[7].orbitType, fetchedSatellites[7].operational);
+
+          // this.sourceList.push(space);
 
 
 
@@ -78,13 +78,3 @@ export class AppComponent {
 //    }.bind(this));
 // }.bind(this));
 
-
-  // constructor() {
-  //   this.sourceList = [
-  //     new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-  //     new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-  //     new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-  //     new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-  //     new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-  //  ];
-  // }
